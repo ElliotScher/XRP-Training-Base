aa86b2ab8c87899fb84391a43cbaca081fba0702
ElliotScher <ecscher84@gmail.com>
2026-09-03 09:19:16 -0400

    add initial code

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.xrp.XRPMotor;
+
+public class Robot extends TimedRobot {
+    private final XRPMotor motor = new XRPMotor(0);
+
+    @Override
+    public void robotPeriodic() {
+
+    }
+}
